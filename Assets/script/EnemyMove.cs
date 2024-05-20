5b629b3b6b65d456d976a9bdca94fbcdf5569595
etigoshun <94036031+etigoshun@users.noreply.github.com>
2024-05-20 14:10:46 +0900

    敵の追加

--- Assets/script/EnemyMove.cs
+++ Assets/script/EnemyMove.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
+    public GameObject goal;
+
+    public NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+
+        goal = GameObject.Find("player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = goal.transform.position;
     }
 }

22a9f19fb43dfe2915c8ed6c2089e52edb030249
etigoshun <94036031+etigoshun@users.noreply.github.com>
2024-05-29 13:04:25 +0900

    敵の巡回挙動の追加(未完成)

--- Assets/script/EnemyMove.cs
+++ Assets/script/EnemyMove.cs
@@ -5,21 +5,41 @@ using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
-    public GameObject goal;
+
+    public Transform[] goals;
+
+    private int destNum = 0;
 
     public NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        //agent = GetComponent<NavMeshAgent>();
 
-        goal = GameObject.Find("player");
+        agent.destination = goals[destNum].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = goal.transform.position;
+        if(agent.remainingDistance < 0.1f)
+        {
+            NextGoal();
+        }
+    }
+
+    void NextGoal()
+    {
+        destNum += 1;
+
+        if(destNum == 3)
+        {
+            destNum = 0;
+        }
+
+        agent.destination = goals[destNum].position;
+
+        Debug.Log(destNum);
     }
 }

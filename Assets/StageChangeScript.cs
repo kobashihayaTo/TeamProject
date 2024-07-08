64591210fbb62072b19619f809fcee9103ebade3
kobashihayaTo <k021g1128@g.neec.ac.jp>
2024-07-08 12:15:47 +0900

    シーン切り替えの時にアニメーション入れようとした

--- Assets/StageChangeScript.cs
+++ Assets/StageChangeScript.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class StageChangeScript : MonoBehaviour
 {
+    //ゲーム画面のUI
+    //[SerializeField] CanvasGroup grInfo;
+    //メニュー画面のUI
+    //[SerializeField] CanvasGroup grMenu;
+    //四角のTransform
+    [SerializeField] Transform squareA;
+    [SerializeField] Transform squareB;
+    [SerializeField] Transform squareC;
+    //マスクのTransform
+    [SerializeField] Transform squareMesk;
+
+
     [SerializeField]
     Material[] materialArray = new Material[2];
 
     Material changeMaterial;
-
-    
 
     public int setCount;
     int count = 0;
@@ -29,7 +40,7 @@ public class StageChangeScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             count++;
-            
+
             if (count > 2)
             {
                 count = 2;
@@ -40,7 +51,7 @@ public class StageChangeScript : MonoBehaviour
         {
             count--;
 
-            if (count < 0) 
+            if (count < 0)
             {
                 count = 0;
             }
@@ -48,42 +59,8 @@ public class StageChangeScript : MonoBehaviour
         }
 
         //ステージ選択画面のボックスの色を変える
-        if(count == setCount)
-        {
-            colorCount = 1;
-
-            //countが0なら左のボックスを赤に変える
-            GetComponent<MeshRenderer>().material = materialArray[colorCount];
-        }
-        else
-        {
-            //それ以外なら白にする
-            colorCount = 0;
-
-            //countが0なら左のボックスを赤に変える
-            GetComponent<MeshRenderer>().material = materialArray[colorCount];
-        }
-
         if (count == setCount)
         {
-            //countが1なら左のボックスを赤に変える
-            colorCount = 1;
-
-            //countが0なら左のボックスを赤に変える
-            GetComponent<MeshRenderer>().material = materialArray[colorCount];
-        }
-        else
-        {
-            //それ以外なら白にする
-            colorCount = 0;
-
-            //countが0なら左のボックスを赤に変える
-            GetComponent<MeshRenderer>().material = materialArray[colorCount];
-        }
-
-        if (count == setCount)
-        {
-            //countが2なら左のボックスを赤に変える
             colorCount = 1;
 
             //countが0なら左のボックスを赤に変える
@@ -100,18 +77,38 @@ public class StageChangeScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-           if(count==0)
+            if (count == 0)
             {
+                ScreenTransition();
                 SceneManager.LoadScene("Stage1Scene");
             }
-           else if(count==1)
+            else if (count == 1)
             {
-
+                ScreenTransition();
             }
-           else if(count==2)
+            else if (count == 2)
             {
-
+                ScreenTransition();
             }
         }
+       // ScreenTransition();
+    }
+
+    private void ScreenTransition()
+    {
+        var sequence = DOTween.Sequence();
+        sequence = DOTween.Sequence()
+            .Append(squareA.DOLocalMoveX(0f, 2f))
+            .Join(squareB.DOLocalMoveX(0f, 2f).SetDelay(0.5f))
+            .Join(squareC.DOLocalMoveX(0f, 2f).SetDelay(0.5f))
+            .Join(squareMesk.DOLocalMoveX(0f, 2f).SetDelay(0.5f))
+
+            .AppendCallback(() =>
+            {
+                squareA.localPosition = new Vector3(-18f, 0f, 0f);
+                squareB.localPosition = new Vector3(-18f, 0f, 0f);
+                squareC.localPosition = new Vector3(-18f, 0f, 0f);
+                squareMesk.localPosition = new Vector3(-18f, 0f, 0f);
+            });
     }
 }
